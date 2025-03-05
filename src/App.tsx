71ca67647c44e72a3a7e63284bde47070d660d6f
alokified <alokshah951@gmail.com>
2025-03-05 17:27:13 +0530

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ function App() {
                   <img src="https://zojqhjtepbctgmqurctj.supabase.co/storage/v1/object/public/images/1741170129943-download-removebg-preview%20(7)%201.png" alt="Pizza" />
                 </div>
                 <div className="food-item">
-                  <Utensils size={40} color="#ffffff" />
+                  <img src="https://zojqhjtepbctgmqurctj.supabase.co/storage/v1/object/public/images/1741170129943-download-removebg-preview%20(2)%201.png" alt="item" />
                 </div>
               </div>
             </div>
